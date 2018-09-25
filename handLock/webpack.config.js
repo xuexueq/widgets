8a31bd78002ec78eb75936e365257e094d6ba5d2
Xue QiaoLing <i-xueqiaoling@mobike.com>
2018-09-25 11:20:39 +0800

    init project

---
+++ handLock/webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        }, {
+            test: /\.less$/,
+            exclude: /node_modules/,
+            use: ExtractTextPlugin.extract(['css-loader', 'less-loader'])
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: ExtractTextPlugin.extract(['css-loader', 'postcss-loader'])
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html'
+        }),
+        new ExtractTextPlugin('[name].css')
+    ],
+    devServer: {
+        contentBase: './dist'
+    }
+};
